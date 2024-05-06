34c3068e9be3e67a95da3b9052e2861fb6ff2de3
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-06 11:37:47 -0300

    react

---
+++ cineapp/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Componentes/Header"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <h1>GamaNet</h1>
+    </>
+  )
+}
+
+export default App
---
+++ cineapp/src/Pages/PagNotFound.jsx
@@ -0,0 +1,5 @@
+function PagNotFound() {
+    return ( <h1>Erro 404</h1> );
+}
+
+export default PagNotFound;
